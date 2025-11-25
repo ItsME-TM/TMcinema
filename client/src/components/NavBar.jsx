abfa11ca8f565e71472f20f0d22d3ab987dcc017
ItsME-TM <thushanmalaka123@gmail.com>
2025-11-25 18:20:33 +0530

    Add Clerk integration and fix Home component typo; update NavBar icons

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { assets } from "../assets/assets";
-import { Menu, SearchIcon, XIcon } from "lucide-react";
+import { Menu, Search, X } from "lucide-react";
 
 function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -15,7 +15,7 @@ function NavBar() {
       max-md:text-lg z-50 flex flex-col md:flex-row items-center max-md:justify-center
       gap-8 md:px-8 py-3 max-md:h-screen md:rounded-full backdrop-blur bg-black/70 md:bg-white/10
       md:border border-gray-300/20 overflow-hidden transition-[width] duration-300 ${isOpen ? 'max-md:w-full':'max-md:w-0'}`} >
-        <XIcon className="md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer" 
+        <X className="md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer" 
         onClick={() => setIsOpen(!isOpen)}/>
         <Link to="/"  onClick={() => {scrollTo(0,0), setIsOpen(false)}}>Home</Link>
         <Link to="/movies" onClick={() => {scrollTo(0,0), setIsOpen(false)}}>Movies</Link>
@@ -25,7 +25,7 @@ function NavBar() {
       </div>
 
       <div className="flex items-center gap-8">
-        <SearchIcon className="max-md:hidden w-6 h-6 cursor-pointer"/>
+        <Search className="max-md:hidden w-6 h-6 cursor-pointer"/>
         <button className="px-4 py-1 sm:px-7 sm:py-2 bg-primary 
         hover:bg-primary-dull transition rounded-full font-medium cursor-pointer">Login</button>
       </div>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className="text-3l font-bold text-red-400">
+    <div className="text-3xl font-bold text-red-400">
         
     </div>
   )
